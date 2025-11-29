43e4395e173e9b3e56c66d72e3626de199cb5f57
tinkerbytes <tsramsey001@gmail.com>
2025-11-30 00:05:29 +0200

    Initialize 'Box' with a valid non-null pointer

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,23 @@
-use std::ptr::NonNull;
+use std::ptr::{self, NonNull};
+use std::alloc::{alloc, dealloc, Layout, handle_alloc_error};
 
 pub struct Box<T> {
     ptr: NonNull<T>
+}
+
+impl<T> Box<T> {
+    /// initialize our 'Box' with value 'T'
+    pub fn new(value: T) -> Self {
+        let layout = Layout::new::<T>();
+        let raw_ptr = unsafe { alloc(layout) } as *mut T;
+
+        let raw_ptr = match NonNull::<T>::new(raw_ptr) {
+            Some(ptr) => ptr,
+            None => handle_alloc_error(layout),
+        };
+
+        unsafe { ptr::write(raw_ptr.as_ptr(), value) }
+
+        Box { ptr: raw_ptr }
+    }
 }
